27b8264d01e059235006cd986f4954a573091a6e
Oumar <foumar404@gmail.com>
2023-03-23 13:08:24 +0100

    first commit

---
+++ bin/dart_apprentice_beyond_learn.dart
@@ -0,0 +1,5 @@
+import 'package:dart_apprentice_beyond_learn/dart_apprentice_beyond_learn.dart' as dart_apprentice_beyond_learn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_apprentice_beyond_learn.calculate()}!');
+}
---
+++ test/dart_apprentice_beyond_learn_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_apprentice_beyond_learn/dart_apprentice_beyond_learn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
