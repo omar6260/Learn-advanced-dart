e71d62696ec75abe2928e48507c4060e32eb8a59
Oumar Fall <foumar404@gmail.com>
2023-11-01 12:58:21 +0100

    update dart

---
+++ bin/string_manipulation.dart
@@ -0,0 +1,6 @@
+void main() {
+  print(trimmed);
+}
+
+const userInput = '  221B Baker St.  ';
+final trimmed = userInput.trim();
---
+++ bin/todo_model.dart
@@ -0,0 +1,29 @@
+class Todo {
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> jsonMap) {
+    return Todo(
+        userId: jsonMap['userId'] as int,
+        id: jsonMap['id'] as int,
+        title: jsonMap['title'] as String,
+        completed: ['completed'] as bool);
+  }
+
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  @override
+  String toString() {
+    return 'userId: $userId\n'
+        'id: $id\n'
+        'title: $title\n'
+        'completed: $completed\n';
+  }
+}
